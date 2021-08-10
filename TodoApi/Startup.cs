2da8cd8e9c7033981ca69f1d34a2f53b5da1e9c7
Irtiza <irtizahussain.edu098@gmail.com>
2021-08-10 18:20:30 +0500

    Second Commit

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -21,7 +21,7 @@ namespace TodoApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
+            services.AddSwaggerGen();
             services.AddDbContext<TodoContext>(opt =>
                                                opt.UseInMemoryDatabase("TodoList"));
             //services.AddSwaggerGen(c =>
@@ -44,6 +44,14 @@ namespace TodoApi
 
             app.UseAuthorization();
 
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
